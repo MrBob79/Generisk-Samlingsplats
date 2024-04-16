7d1a45d966385674ca8c04df22832585ff315dbc
MrBob79 <magnus.martensson79@gmail.com>
2024-04-16 22:26:15 +0200

    Add project files.

---
+++ Generisk Samlingsplats/Aktiepost.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class Bankkonto
+{
+    // Attribut
+    private string Owner;
+    private double Saldo;
+    private int AccountNumber;
+    private string Stock;
+    private double Savings;
+
+    // Konstruktor
+    public Bankkonto(string owner, double saldo, int accountNumber, double Saves)
+    {
+        this.Owner = owner;
+        this.Saldo = saldo;
+        this.AccountNumber = accountNumber;
+        this.Stock = " ";
+        this.Savings = Saves;
+        Console.WriteLine("-----------------------------\n");
+    }
+
+    // Metod för att sätta in pengar
+    public void CashIn(double cash)
+    {
+        this.Saldo += cash;
+        Console.WriteLine($"{cash}Kr har satts in på kontot!");
+        Console.WriteLine("-----------------------------\n");
+    }
+
+    // Metod för att ta ut pengar
+    public void CashOut(double cash)
+    {
+        if (cash <= this.Saldo)
+        {
+            this.Saldo -= cash;
+            Console.WriteLine($"{cash}Kr har tagits ut från kontot");
+            Console.WriteLine("-----------------------------\n");
+        }
+        else
+        {
+            Console.WriteLine("Otillräckligt saldo för att göra uttag!!");
+            Console.WriteLine("-----------------------------\n");
+        }
+    }
+
+    // Metod för att sätta in månadssparande 
+    public void MontlySavings(double Saves)
+    {
+        this.Savings = Saves;
+        Console.WriteLine($"Månadssparande {Savings}Kr");
+        Console.WriteLine("-----------------------------\n");
+
+    }
+    public void StockSaves(string stock)
+    {
+        this.Stock = stock;
+        Console.WriteLine($"Vald Aktie: {stock}");
+        Console.WriteLine("-----------------------------\n");
+    }
+
+    // Metod för att skriva ut Bankinfo
+    public void PrintInfo()
+    {
+        Console.WriteLine($"\nKontos Ägare: {this.Owner}");
+        Console.WriteLine($"Saldo: {this.Saldo}");
+        Console.WriteLine($"KontoNrummer: {this.AccountNumber}");
+        Console.WriteLine($"Månadssparande: {this.Savings}");
+        Console.WriteLine($"Aktie: {this.Stock}");
+
+    }
+}
---
+++ Generisk Samlingsplats/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Skapar inmatning för ett nytt konto
+        Console.WriteLine("Skriv in ditt Namn som ägare för kontot:");
+        string Owner = Console.ReadLine();
+
+        Console.WriteLine("Skriv in ditt nuvarande Saldo:");
+        double Saldo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Skriv in ditt kontonummer: ");
+        int accountNumber = int.Parse(Console.ReadLine());
+
+        // Användaren anger hur mycket man vill månadsspara
+        Console.WriteLine("Skriv hur mycket du vill månadsspara: ");
+        double Saves = double.Parse(Console.ReadLine());
+
+        Bankkonto konto = new Bankkonto(Owner, Saldo, accountNumber, Saves);
+
+        // Sätt in pengar
+        Console.WriteLine("Ange hur mycket du vill sätta in: ");
+        double deposit = double.Parse(Console.ReadLine());
+        konto.CashIn(deposit);
+
+        // Ta ut pengar
+        Console.WriteLine("Ange hur mycket pengar du vill ta ut: ");
+        double insert = double.Parse(Console.ReadLine());
+        konto.CashOut(insert);
+
+
+        // Välja Aktie att månadsspara i
+        Console.WriteLine("Ange i vilken Aktie du vill spara: ");
+        string Stocks = Console.ReadLine();
+        konto.StockSaves(Stocks);
+
+        konto.PrintInfo();
+
+    }
+}
+
+
